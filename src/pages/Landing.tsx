d5d560dc00de822ab3fdfac5087d417c2f2534ca
Twinemukama <twinemukamai@gmail.com>
2026-03-23 11:42:23 +0300

    ghj

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,6 @@
-import { motion } from "framer-motion";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import profileImg from "@/assets/profile.jpg";
 
 const Landing = () => {
